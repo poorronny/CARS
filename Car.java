7015ecd3075daa9c335e12b05572e6401b420fc2
ronny <ronny_migler@mail.ru>
2022-11-20 23:17:27 +0300

    Done

--- Car.java
+++ Car.java
@@ -6,22 +6,46 @@ public class Car {
     int year;
     String country;
 
-    Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
-
-    }
     @Override
     public String toString() {
-        return brand + " " +  model +
+        return brand + " " + model +
                 ", engine volume: " + engineVolume +
                 ", color: " + color +
                 ", year: " + year +
                 ", country: " + country +
                 ".";
+    }
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        if (brand == null) {
+            this.brand = "Default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "Default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "White";
+        } else {
+            this.color = color;
+        }
+        if (year <= 2000) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (country == null) {
+            this.country = "Default";
+        } else {
+            this.country = country;
+        }
+
     }
 }
