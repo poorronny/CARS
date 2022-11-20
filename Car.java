5939fcb3d10920144633fb5c67a8abd82e4814f5
ronny <ronny_migler@mail.ru>
2022-11-20 19:16:19 +0300

    готовое дз

--- Car.java
+++ Car.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+
+    }
+    @Override
+    public String toString() {
+        return brand + " " +  model +
+                ", engine volume: " + engineVolume +
+                ", color: " + color +
+                ", year: " + year +
+                ", country: " + country +
+                ".";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Car ladaGranta = new Car("LADA", "Granta", 1.7, "yellow", 2015, "Russia");
+        Car audi = new Car ("AUDI", "A8 50 L TDI quattro", 3.0, "black", 2020, "Germany");
+        Car bmw = new Car ("BMW", "Z8", 3.0, "black", 2021, "Germany");
+        Car kia = new Car ("KIA", "Sportage 4th gen", 2.4, "red", 2018, "South Korea");
+        Car hyundai = new Car ("HYUNDAI", "Avante", 1.6, "orange", 2016, "South Korea");
+
+        System.out.println(ladaGranta);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
+
+    }
+
 }
+
+
